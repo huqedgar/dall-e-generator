a99f5f913e7fe8477c1a2c3ddbab86be9e6d8556
Hưng <96417124+huqedgar@users.noreply.github.com>
2023-09-23 15:27:06 +0700

    set-up package

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import classNames from 'classnames/bind';
+import styles from './App.scss';
+
+const cx = classNames.bind(styles);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div className={cx('App')}>
+         <header className={cx('App-header')}>
+            <p>
+               Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a className={cx('App-link')} href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+               Learn React
+            </a>
+         </header>
+      </div>
+   );
 }
 
 export default App;
